efb1ca4e290b222fa9941bb22c3ac3b1ad85070c
Aman-149 <amanuelfikadu23971@gmail.com>
2025-07-19 21:21:33 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+const textbox = document.getElementById("textbox");
+const toFahrenheit = document.getElementById("toFahrenheit");
+const toCelsius = document.getElementById("toCelsius");
+const result = document.getElementById("result");
+let temp;
+function convert(){
+    let temp = Number(textbox.value);
+
+    if(toFahrenheit.checked){
+        //result.textContent= "you selected Fahrenheit  ";
+        temp = Number(textbox.value)
+        temp = (temp* 9/5)+32;
+        result.textContent =  temp + " °F";
+    }
+    else if (toCelsius.checked) {
+        //result.textContent= "you selected Celsius   ";
+        temp = Number(textbox.value)
+        temp = (temp - 32) * 5/9;
+        result.textContent = temp + " °C";
